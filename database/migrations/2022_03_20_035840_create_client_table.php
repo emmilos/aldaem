ddc615da90158ef92272b333eb99fff4a314a6d5
emmilos <81449171+emmilos@users.noreply.github.com>
2022-04-25 09:52:57 +0000

    migration clients

--- database/migrations/2022_03_20_035840_create_client_table.php
+++ database/migrations/2022_03_20_035840_create_client_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('client', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string("statut_juridique");
             $table->string("adresse");
